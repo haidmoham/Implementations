9ec30b673fa2a77494f9f3eb823a5267227ba4f1
Mohammad Haider <haidmoham@gmail.com>
2016-07-20 15:21:26 -0400

    Adding Random Int Array Generator for creating test cases for sorts

---
+++ src/RandomArrayGenerator.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Mohammad Haider (haidmoham) on 7/20/16.
+ */
+import java.util.*;
+
+public class RandomArrayGenerator {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] ar = new int[n];
+        for (int i = 0; i < n; i++) {
+            ar[i] = (int) (Math.random() * 10000 + 1);
+            System.out.print(ar[i] + ",");
+        }
+    }
+}
