99521045ad055b34babd867d0628bd722d2412fd
Mohammad Haider <haidmoham@gmail.com>
2016-07-19 23:43:23 -0400

    Adding Segment Trees

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -3,7 +3,7 @@
  */
 import java.util.*;
 
-public class BubbleSortArrays {
+public class BubbleSort {
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
