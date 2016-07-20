1b652e226ce1cfd25fb22d96f8af35dfe869ae0e
Mohammad Haider <haidmoham@gmail.com>
2016-07-20 00:09:04 -0400

    Adding QuickSort

---
+++ src/QuickSort.java
@@ -0,0 +1,59 @@
+/**
+ * Created by Mohammad Haider (haidmoham) on 7/20/16.
+ */
+import java.util.*;
+
+public class QuickSort {
+    private int array[];
+    private int length;
+
+    public void sort(int[] inputArr){
+        if (inputArr == null || inputArr.length == 0){
+            return;
+        }
+        this.array = inputArr;
+        length = inputArr.length;
+        quickSort(0, length-1);
+    }
+    private void quickSort(int lowerIndex, int higherIndex){
+        int i = lowerIndex;
+        int j = higherIndex;
+        // calculate the pivot
+        int pivot = array[lowerIndex + (higherIndex-lowerIndex)/2];
+        // divide into two arrays
+        while (i <= j){
+            while (array[i] < pivot){
+                i++;
+            }
+            while (array[j] > pivot){
+                j--;
+            }
+            if (i <= j){
+                exchangeNumbers(i, j);
+                //shift both indices
+                i++;
+                j--;
+            }
+        }
+        if (lowerIndex < j)
+            quickSort(lowerIndex, j);
+        if (i < higherIndex)
+            quickSort(i, higherIndex);
+        for (int k = 0; k < array.length; k++){
+            System.out.print(array[k] + " ");
+        }
+        System.out.println();
+    }
+    private void exchangeNumbers(int i, int j){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    public static void main(String a[]){
+        QuickSort sorter = new QuickSort();
+        int[] input = {24,2,45,20,56,75,2,56,99,53,12};
+        sorter.sort(input);
+        for (int i:input)
+            System.out.print(i + " ");
+    }
+}
