322562d5d7b8932b9cdde1fe6d1d3cd1562f0cad
haidmoham <haidmoham@gmail.com>
2016-08-25 18:05:44 -0400

    Fixed Tries, added FFT Columbia Implementation (which borrows from UIUC(

--- src/Tries.java
+++ src/Tries.java
@@ -8,15 +8,19 @@ public class Tries {
     private class TrieNode {
         Map<Character, TrieNode> children;
         boolean endOfWord;
+
         public TrieNode() {
             children = new HashMap<>();
             endOfWord = false;
         }
     }
+
     private final TrieNode root;
-    public Trie(){
+
+    public Tries() {
         root = new TrieNode();
     }
+
 
     /*Iterative insertion into trie*/
     public void insert(String word){
