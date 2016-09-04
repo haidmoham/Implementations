b239ca939cd907ede32cd35bea85e7b92b82e4ee
haidmoham <haidmoham@gmail.com>
2016-09-03 20:35:13 -0400

    Fast Scanner

---
+++ src/FastScan.java
@@ -0,0 +1,58 @@
+/**
+ * Created by mohammad on 9/3/16.
+ */
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class FastScan {
+    public static void main(String[] args) throws Exception {
+        FastScanner scanner = new FastScanner(System.in);
+        long val = scanner.nextLong();
+        String str = scanner.nextLine();
+
+        System.out.println(val);
+        System.out.println(str);
+    }
+
+    static class FastScanner {
+        private BufferedReader reader = null;
+        private StringTokenizer tokenizer = null;
+
+        public FastScanner(InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+            tokenizer = null;
+        }
+
+        public String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public String nextLine() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    return reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            return tokenizer.nextToken("\n");
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public long nextInt() {
+            return Integer.parseInt(next());
+        }
+
+    }
+}
