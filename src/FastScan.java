853155959a8fa7943a33c5fce1a878cfcc867cfc
Mohammad Haider <haidmoham@gmail.com>
2016-09-14 20:54:51 -0400

    FastScanner Update

--- src/FastScan.java
+++ src/FastScan.java
@@ -6,53 +6,64 @@ import java.util.StringTokenizer;
 
 public class FastScan {
     public static void main(String[] args) throws Exception {
-        FastScanner scanner = new FastScanner(System.in);
-        long val = scanner.nextLong();
-        String str = scanner.nextLine();
+        FastScanner scanner = new FastScanner();
+        long val = scanner.nextLong()
+        String str = scanner.readNextLine();
 
         System.out.println(val);
         System.out.println(str);
     }
 
-    static class FastScanner {
-        private BufferedReader reader = null;
-        private StringTokenizer tokenizer = null;
-
-        public FastScanner(InputStream in) {
-            reader = new BufferedReader(new InputStreamReader(in));
-            tokenizer = null;
+    public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastScanner(Reader in) {
+            br = new BufferedReader(in);
         }
-
-        public String next() {
-            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+        public FastScanner() {
+            this(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
                 try {
-                    tokenizer = new StringTokenizer(reader.readLine());
+                    st = new StringTokenizer(br.readLine());
                 } catch (IOException e) {
-                    throw new RuntimeException(e);
+                    e.printStackTrace();
                 }
             }
-            return tokenizer.nextToken();
+            return st.nextToken();
         }
-
-        public String nextLine() {
-            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
-                try {
-                    return reader.readLine();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-
-            return tokenizer.nextToken("\n");
-        }
-
-        public long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        public long nextInt() {
+        int nextInt() {
             return Integer.parseInt(next());
         }
-
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        String readNextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+        int[] readIntArray(int n) {
+            int[] a = new int[n];
+            for (int idx = 0; idx < n; idx++) {
+                a[idx] = nextInt();
+            }
+            return a;
+        }
+        long[] readLongArray(int n) {
+            long[] a = new long[n];
+            for (int idx = 0; idx < n; idx++) {
+                a[idx] = nextLong();
+            }
+            return a;
+        }
     }
 }
